49da44de39aef54d9c1c3a70e0a54434703463e3
Dondrey Taylor <dondrey.taylor@gmail.com>
2019-04-21 15:28:49 -0400

    Added foundation of nimble website

---
+++ public/assets/js/app/controllers.js
@@ -0,0 +1,7 @@
+'use strict';
+
+angular.module('Application.controllers', [])
+
+.controller("LandingController", ["$scope", function($scope) {
+  
+}])
---
+++ public/assets/js/app/init.js
@@ -0,0 +1,22 @@
+'use strict';
+
+angular.module('Application', [
+  'ngRoute',
+  'Application.controllers'
+]).
+config(['$locationProvider', '$interpolateProvider', '$routeProvider', function($locationProvider, $interpolateProvider, $routeProvider) {
+
+    // URL prefix
+    $locationProvider.hashPrefix('!');
+
+    // Change angular interpolate symbols to prevent
+    // clash with template engine
+    $interpolateProvider.startSymbol('[[');
+    $interpolateProvider.startSymbol(']]');
+
+    // Application routing
+    $routeProvider
+      .when("/", {
+          templateUrl : "/views/pages/landing.html"
+      });
+}]);
